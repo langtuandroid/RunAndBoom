220ae58901f33fd6408502bb856a319188e77cb2
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-03-31 09:35:13 +0600

    Enemy projectiles hit Hero.

--- Assets/CodeBase/Projectiles/Hit/ProjectileHit.cs
+++ Assets/CodeBase/Projectiles/Hit/ProjectileHit.cs
@@ -5,36 +5,18 @@ namespace CodeBase.Projectiles.Hit
 {
     public class ProjectileHit : BaseProjectileHit
     {
-        private void OnTriggerEnter(Collider other)
-        {
-            Debug.Log("OnTriggerEnter");
-            string targetTag = other.gameObject.tag;
-            // Debug.Log($"targetTag {targetTag}");
-            Debug.Log($"gameObject name {other.gameObject.name}");
-
-            if (IsTargetTag(targetTag))
-            {
-                Trail.HideTrace();
-                Movement.Stop();
-                HeroHealth heroHealth = other.gameObject.GetComponent<HeroHealth>();
-                heroHealth.TakeDamage(1);
-            }
-        }
-
         private void OnCollisionEnter(Collision collision)
         {
-            Debug.Log("OnCollisionEnter");
             string targetTag = collision.gameObject.tag;
-            // Debug.Log($"targetTag {targetTag}");
-            Debug.Log($"gameObject name {collision.gameObject.name}");
 
             if (IsTargetTag(targetTag))
             {
-                Trail.HideTrace();
+                if (Trail != null)
+                    Trail.HideTrace();
+
                 Movement.Stop();
                 HeroHealth heroHealth = collision.gameObject.GetComponent<HeroHealth>();
                 heroHealth.TakeDamage(1);
-                // hit.transform.gameObject.GetComponent<IHealth>().TakeDamage(_damage);
             }
         }
     }
