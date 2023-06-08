9631d3d29924aff5d25c5b14f95416f9a0c30118
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-06-08 15:56:55 +0600

    Updated Web Utility plugin. Added muting audio if game in background, else unmuting.

--- Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -1,17 +1,22 @@
 ﻿using CodeBase.Infrastructure.States;
+using CodeBase.UI;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace CodeBase.Infrastructure
 {
     public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
     {
-        public LoadingCurtain CurtainPrefab;
+        [SerializeField] private LoadingCurtain _curtainPrefab;
+        [SerializeField] private AudioBackgroundChanger _audioBackgroundChangerPrefab;
+
         private Game _game;
 
         private void Awake()
         {
-            _game = new Game(this, Instantiate(CurtainPrefab));
+            _game = new Game(this, Instantiate(_curtainPrefab));
             _game.StateMachine.Enter<BootstrapState>();
+            Instantiate(_audioBackgroundChangerPrefab);
 
             DontDestroyOnLoad(this);
         }
---
+++ Assets/CodeBase/UI/AudioBackgroundChanger.cs
@@ -0,0 +1,41 @@
+﻿using Agava.WebUtility;
+using CodeBase.Services;
+using CodeBase.Services.PersistentProgress;
+using Plugins.SoundInstance.Core.Static;
+using UnityEngine;
+
+namespace CodeBase.UI
+{
+    public class AudioBackgroundChanger : MonoBehaviour
+    {
+        private IPlayerProgressService _playerProgressService;
+
+        private void Awake()
+        {
+            _playerProgressService = AllServices.Container.Single<IPlayerProgressService>();
+            DontDestroyOnLoad(this);
+        }
+
+        private void OnEnable() =>
+            WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
+
+        private void OnDisable() =>
+            WebApplication.InBackgroundChangeEvent -= OnInBackgroundChange;
+
+        private void OnInBackgroundChange(bool inBackground)
+        {
+            if (inBackground)
+            {
+                SoundInstance.StopRandomMusic(false);
+                SoundInstance.musicVolume = Constants.Zero;
+                SoundInstance.GetMusicSource().volume = Constants.Zero;
+            }
+            else
+            {
+                SoundInstance.ResumeMusic();
+                SoundInstance.GetMusicSource().volume = _playerProgressService.Progress.SettingsData.MusicVolume;
+                SoundInstance.musicVolume = _playerProgressService.Progress.SettingsData.MusicVolume;
+            }
+        }
+    }
+}
