adc7dfd95e8d8d8fc2eb90b7ab23086d5596f5fe
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-06-19 18:18:49 +0600

    Changed for UI images compress type. Fixed HpBar current image.

--- Assets/CodeBase/UI/Elements/Hud/HpBar.cs
+++ Assets/CodeBase/UI/Elements/Hud/HpBar.cs
@@ -5,9 +5,9 @@ namespace CodeBase.UI.Elements.Hud
 {
     public class HpBar : MonoBehaviour
     {
-        [SerializeField] private Image _currentImage;
+        [SerializeField] private Slider _slider;
 
         public void SetValue(float current, float max) =>
-            _currentImage.fillAmount = current / max;
+            _slider.value = current / max;
     }
 }
