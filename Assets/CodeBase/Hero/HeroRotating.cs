86ba2f0595fe5185a1921b0699d23c06d8f32d0f
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-07-13 17:20:56 +0600

    Fixed hero rotation angle.

--- Assets/CodeBase/Hero/HeroRotating.cs
+++ Assets/CodeBase/Hero/HeroRotating.cs
@@ -1,31 +1,19 @@
-using CodeBase.Services;
-using CodeBase.Services.Input;
 using UnityEngine;
 
 namespace CodeBase.Hero
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class HeroRotating : MonoBehaviour
     {
-        [SerializeField] private Camera _camera;
-        [SerializeField] private float _verticalSensitivity = 2.0f;
-        [SerializeField] private float _horizontalSensitivity = 2.0f;
-        [SerializeField] private float _edgeAngle = 87f;
+        [SerializeField] private float _sensitivity = 1.0f;
 
-        private IInputService _inputService;
+        private const float EdgeAngle = 87f;
+
         private float _xAxisClamp = 0;
         private bool _canRotate = true;
-        private float _verticalRotation;
 
-        private void Awake() =>
-            _inputService = AllServices.Container.Single<IInputService>();
-
-        private void Start()
-        {
-            if (AllServices.Container.Single<IInputService>() is DesktopInputService)
-                Cursor.lockState = CursorLockMode.Locked;
-            else
-                Cursor.lockState = CursorLockMode.Confined;
-        }
+        private void Start() =>
+            Cursor.lockState = CursorLockMode.Locked;
 
         private void Update()
         {
@@ -35,21 +23,32 @@ namespace CodeBase.Hero
 
         private void Rotate()
         {
-            if (_inputService.LookAxis.sqrMagnitude > Constants.Epsilon)
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            float rotationAmountX = mouseX * _sensitivity;
+            float rotationAmountY = mouseY * _sensitivity;
+
+            _xAxisClamp -= rotationAmountY;
+
+            Vector3 rotation = transform.rotation.eulerAngles;
+
+            rotation.x -= rotationAmountY;
+            rotation.y += rotationAmountX;
+
+            switch (_xAxisClamp)
             {
-                RotateHorizontal();
-                RotateVertical();
+                case > EdgeAngle:
+                    _xAxisClamp = EdgeAngle;
+                    rotation.x = EdgeAngle;
+                    break;
+                case < -EdgeAngle:
+                    _xAxisClamp = -EdgeAngle;
+                    rotation.x = -EdgeAngle;
+                    break;
             }
-        }
 
-        private void RotateHorizontal() =>
-            transform.Rotate(Vector3.up * _inputService.LookAxis.x * _horizontalSensitivity);
-
-        private void RotateVertical()
-        {
-            _verticalRotation -= _inputService.LookAxis.y;
-            _verticalRotation = Mathf.Clamp(_verticalRotation, -_edgeAngle, _edgeAngle);
-            _camera.transform.localRotation = Quaternion.Euler(_verticalRotation * _verticalSensitivity, 0, 0);
+            transform.rotation = Quaternion.Euler(rotation);
         }
 
         public void TurnOn() =>
