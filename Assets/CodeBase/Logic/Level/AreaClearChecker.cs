22569d52ef952bde6b7c83abdf15046c0b72c463
Stanislav Anishchenko <anischenko_stas@mail.ru>
2024-01-09 21:49:45 +0600

    Moved weapons container left from camera.

---
+++ Assets/CodeBase/Logic/Level/AreaClearChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CodeBase.Enemy;
+using CodeBase.Logic.EnemySpawners;
+using UnityEngine;
+
+namespace CodeBase.Logic.Level
+{
+    public class AreaClearChecker : MonoBehaviour
+    {
+        [SerializeField] private List<SpawnMarker> _spawnMarkers;
+
+        private List<EnemyHealth> _enemyHealths;
+        
+        
+    }
+}
